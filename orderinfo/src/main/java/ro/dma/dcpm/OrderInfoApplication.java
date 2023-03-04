ba81262c47952391be771137679101dd7004ede8
Dan Gabriel Pavel <dgpavel@gmail.com>
2023-03-04 20:45:41 +0200

    Implementare View Databases Pattern pentru microserviciul OrderInfo

--- orderinfo/src/main/java/ro/dma/dcpm/OrderInfoApplication.java
+++ orderinfo/src/main/java/ro/dma/dcpm/OrderInfoApplication.java
@@ -2,10 +2,8 @@ package ro.dma.dcpm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class OrderInfoApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderInfoApplication.class, args);
